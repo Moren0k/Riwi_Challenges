6094224cf98e180b31636e738e613d000330c0e6
Moren0k <jhoskevinagudelomoreno@gmail.com>
2025-04-25 13:54:11 -0500

    Check

---
+++ Verf_Password/README.md
@@ -0,0 +1,13 @@
+# Verificar contraseña segura
+
+### Descripción:
+
+Crea una función llamada es_contrasena_segura que reciba un texto (string) como parámetro y devuelva True si la contraseña es segura, o False si no lo es.
+
+Una contraseña se considera segura si cumple con lo siguiente:
+
+    Tiene al menos 8 caracteres.
+
+    Contiene al menos una letra mayúscula.
+
+    Contiene al menos un número.
---
+++ Verf_Password/Verf_Password_Segura.py
@@ -0,0 +1,21 @@
+def verf_password_segura():
+    segura = False
+
+    while not segura:
+        passw = input("Ingresa una contraseña Segura: ")
+        longitud = len(passw)
+
+        longitudmin = 8 #Longitud minima de caracteres
+        tiene_mayus = any(letra.isupper() for letra in passw)
+        tiene_num = any(letra.isdigit() for letra in passw)
+
+        if longitud <= longitudmin:
+            print("La contraseña debe tener más de 8 caracteres")
+        elif not tiene_mayus:
+            print("Debe tener almenos una letra mayúscula")
+        elif not tiene_num:
+            print("Debe tener almenos un número")
+        else:
+            print("¡Contraseña Segura!")
+            segura = True
+verf_password_segura()

fa6657dc6e74b997257d62a09d95a2a3bc7bb7e4
Moren0k <jhoskevinagudelomoreno@gmail.com>
2025-06-16 13:30:37 -0500

    Feat: add JavaScript

---
+++ JavaScript/Semana 1/variables.js
@@ -0,0 +1,35 @@
+/*Gestionar el nombre de una empresa: Crear una variable de texto para
+almacenar el nombre de mi empresa favorita (por ejemplo, "Google"). Luego,
+quiero ver ese nombre impreso en pantalla.*/
+
+let company = "Google"
+console.log(company)
+
+/*Contar productos en inventario: Declarar una variable numérica entera para
+registrar la cantidad de productos en el almacén, fijándola en 150. Después de
+asignarla, necesito imprimir esa cantidad para una revisión rápida.*/
+
+let inventory = 150
+console.log(inventory)
+
+/*Establecer un precio base: Definir una variable numérica decimal para
+representar el precio unitario de un artículo, con un valor de 29.99. Quiero poder
+imprimir este precio para futuras referencias.*/
+
+let price = 29.99
+console.log(price)
+
+/*Indicar el estado de un recurso: Usar una variable booleana llamada activo para
+señalar si un sistema o usuario está actualmente True (activo) o False (inactivo).
+Necesito imprimir su estado actual.*/
+
+let asset = true
+console.log(activo)
+
+/*Actualizar una puntuación: Empezar con una variable puntos con el valor inicial
+de 100. Si el usuario gana más puntos, necesito cambiar su valor a 120 y luego
+imprimir el nuevo total de puntos.*/
+
+let points = 100
+points = + 20
+console.log(points)
--- Paper_Test.py
+++
@@ -1,5 +0,0 @@
-"""
-#Pendiente
-Completar codigo de API_Key
-Crear codigo de traga monedas
-"""

0920d17c2046e6c2fe2c3b2ba8bb257ba505ef62
Pablo <pablojimora@gmail.com>
2025-08-05 13:51:43 -0500

    tienda

--- DataBase/NoSQL/pizza-api-nosql/index.js
+++ DataBase/NoSQL/pizza-api-nosql/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const clientes = require('./models/cliente')
 require('dotenv').config();
 
 const app = express();
@@ -8,7 +9,49 @@ app.use(express.json());
 //Conexón
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log('Conectado a mongo'))
-.catch(err => console.error('Error en la conexion', err))
+.catch(err => console.error('Error en la conexion', err));
+
+//Endpoint encargado de insertar un nuevo cliente
+app.post('/cliente', async(req,res)=>{
+    try {
+        const nuevoCliente = new clientes(req.body);
+        const clienteGuardado = await nuevoCliente.save();
+        res.status(201).json(clienteGuardado);
+    } catch (error){
+        console.error('Error al guardar el cliente', error)
+        res.status(400).json({ error: "Error al guardar el cliente" });
+    }
+});
+
+//Mostrar todos los datos
+app.get('/cliente', async (req, res) =>{
+    try {
+        const obtenerClientes = await clientes.find();
+        res.json(obtenerClientes);
+    } catch (error) {
+        console.error('Error al obtener el cliente', error)
+        res.status(500).json({error: 'Error al obtener los clientes'})
+    }
+});
+
+//Actualizar por el ID
+app.put('/cliente/:id', async(req, res) =>{
+    try {
+        const clienteActualizado = await clientes.findByIdAndUpdate(
+            req.params.id,{
+                nombre: req.body.nombre,
+                edad: req.body.edad,
+                direccion: req.body.direccion
+            },
+            {new : true}
+        );
+        if (!clienteActualizado) {
+            return res.status(404).json({error: 'Cliente no encontrado'});
+        }
+        res.json(clienteActualizado);
+    } catch (err){
+        res.status(400).json({error: 'Error al actualizar el cliente'})};
+});
 
 //Port
 const PORT = process.env.PORT || 3000;
--- DataBase/NoSQL/pizza-api-nosql/models/client.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const cliente = new mongoose.Schema({
-    nombre: String,
-    edad: Number,
-    direccion: String,
-});
-
-mongoose.exports = mongoose.model('cliente', cliente);

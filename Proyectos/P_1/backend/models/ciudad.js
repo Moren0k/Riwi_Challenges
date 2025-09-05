b5241083a24e3ae2bea0a7d2185563d8618f34b1
Moren0k <jhoskevinagudelomoreno@gmail.com>
2025-09-05 07:22:47 -0500

    R

--- Proyectos/P_1/backend/models/ciudad.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const cuidadSchema = new mongoose.Schema({
-    nombre : {type: String, required: true}
-});
-
-module.exports = mongoose.model('cuidad', cuidadSchema);

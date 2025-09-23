bb60789891e2789cb46e29cee0cfa79846bf4214
Moren0k <jhoskevinagudelomoreno@gmail.com>
2025-09-23 10:57:40 -0500

    POO

---
+++ C Sharp/POO/Poo/Poo/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Poo;
+public static class Program
+{
+    public static void Main()
+    {
+        var gerrero = new Herencia.Guerrero("kevin", 50, 90, 60, 100, 10, "Oro");
+        gerrero.Turno();
+    }
+}

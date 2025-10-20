f5b60d804ae1211b0398062fb563d71d621aa9d9
Moren0k <jhoskevinagudelomoreno@gmail.com>
2025-10-20 10:52:54 -0500

    WebApi.Test

---
+++ C Sharp/WebApis/WebApplication1/WebApplication1/Models/Person.cs
@@ -0,0 +1,13 @@
+namespace WebApplication1.Models;
+
+public class Person
+{
+    public long Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string SecondLastName { get; set; } = string.Empty;
+    public int Age { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public bool EmailConfirmed { get; set; }
+}
